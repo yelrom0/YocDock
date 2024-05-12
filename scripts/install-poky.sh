641c36cccc734ab58e845ba0b3872d423e0114ee
yelrom0 <yelrom0@gmail.com>
2024-05-12 20:18:29 +1000

    fix(Scripts): poky install script separate repo clone and checkout

--- scripts/install-poky.sh
+++ scripts/install-poky.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cd /
-git clone --depth 1 -b $YOCTO_RELEASE git://git.yoctoproject.org/poky
+echo "Cloning the Poky repository..."
+git clone git://git.yoctoproject.org/poky
 cd /poky
+echo "Checking out the $YOCTO_RELEASE branch and pulling changes..."
+git checkout -t origin/$YOCTO_RELEASE -b $YOCTO_RELEASE
+git pull
+echo "Initializing the build environment..."
 source oe-init-build-env
