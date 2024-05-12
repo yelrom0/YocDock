b01e19dbaf5a7e45ea04d27049a11a7f3d738720
yelrom0 <yelrom0@gmail.com>
2024-05-12 15:43:42 +1000

    chore(Run Script): check if base image exists and build it if not

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Check if the base image exists
+echo "Checking if the base image exists..."
+if [[ "$(docker images -q yocdock-base 2> /dev/null)" == "" ]]; then
+    echo "Base image not found. Building it..."
+    docker-compose build yocdock-base
+fi
+
 if [[ "$1" == "--dev" ]] || [[ "$1" == "-d"]]; then
+    echo "Running the development environment..."
     docker-compose up -d yocdock-dev
 elif [[ "$1" == "--build" ]] || [[ "$1" == "-b" ]]; then
+    echo "Building the image(s)..."
     docker-compose run --rm yocdock-build
 elif [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
     echo "Usage: ./run.sh [OPTION]"
