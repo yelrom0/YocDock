22ab2ad8e6e45a5fff96c3d75dc24dccbd1b672c
yelrom0 <yelrom0@gmail.com>
2024-05-13 00:36:27 +1000

    fix(Run Toaster Script): init bind mount + loop to stop prem exit

--- scripts/run-toaster.sh
+++ scripts/run-toaster.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mkdir -p ~/pocky
-source oe-init-build-env ~/pocky
-source toaster start
+mkdir -p /workdir/pocky
+source oe-init-build-env /workdir/pocky
+source toaster start webport=0.0.0.0:8000
+
+while true; do
+    sleep 1
+done
