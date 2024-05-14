5d4143407644040901c4bd58e393fcba645b7eeb
yelrom0 <yelrom0@gmail.com>
2024-05-14 16:14:28 +1000

    fix(Run Toaster): Move full poky dir over to workdir

--- scripts/run-toaster.sh
+++ scripts/run-toaster.sh
@@ -3,9 +3,10 @@
 git config --global --add safe.directory /poky
 git checkout -t origin/$YOCTO_RELEASE -b $YOCTO_RELEASE
 git pull
-mkdir -p /workdir/poky
-# cd /workdir/poky
-source oe-init-build-env /workdir/poky
+cp -r /poky /workdir/poky
+
+cd /workdir/poky
+source oe-init-build-env
 source toaster start webport=0.0.0.0:8000
 
 while true; do
