0da1fef3295faafdaffc5bca39eb2fc8b36dff2e
yelrom0 <yelrom0@gmail.com>
2024-05-14 14:12:25 +1000

    wip(Run Toaster Script): debugging

--- scripts/run-toaster.sh
+++ scripts/run-toaster.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-mkdir -p /workdir/pocky
-source oe-init-build-env /workdir/pocky
+#mkdir -p /workdir/pocky
+cd /poky
+echo $(ls /)
+source oe-init-build-env /poky
 source toaster start webport=0.0.0.0:8000
 
 while true; do
