c4d1c0456916085c13e8739ef64da68dbab24a9c
yelrom0 <yelrom0@gmail.com>
2024-05-14 15:26:30 +1000

    feat(Run Toaster Script): Pull latest from selected release

--- scripts/run-toaster.sh
+++ scripts/run-toaster.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mkdir -p /workdir/pocky
-source oe-init-build-env /workdir/pocky
+git config --global --add safe.directory /poky
+git checkout -t origin/$YOCTO_RELEASE -b $YOCTO_RELEASE
+git pull
+mkdir -p /workdir/poky
+# cd /workdir/poky
+source oe-init-build-env /workdir/poky
 source toaster start webport=0.0.0.0:8000
 
 while true; do
